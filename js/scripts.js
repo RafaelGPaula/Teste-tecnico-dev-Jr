6460e676bb363edffe787670965ac77e5668406f
Rafael Gonzaga <rafael.gonzaga@hotmail.com.br>
2019-02-01 02:27:19 -0200

    Comit final
    Comit Final, projeto finalizado.

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,71 @@
 function Gerar(){
   	
-	  	//Receber o valor de campos a serem gerados
+	  	//Receber o número de campos a serem gerados
 	  	camp = document.getElementById('TxtCampos').value;
 	  		
-		//validar a execução da Função Gerar()
-		//window.alert("Serão gerados " + camp + " Campos");
-	  		
-		//Loop de Repetição para gerar os campos dinamicamente
-		for (var i = 1; i <= camp; i++) {
+		//Validar se o valor inserido é do tipo numérico.
+		if (!isNaN(camp)) {
+
+				//Loop de Repetição para gerar os campos dinamicamente
+				for (var i = 1; i <= camp; i++) {
+						
+						//Gerar novo campo com id padrão "IdNull"	
+					    document.write('<div class=""><h4 class="Center">Insira o valor:</h4></br><input type="text" id="IdNull" class="css-input inputCampos"></div>');
+											    
+					    //Redefinir o id do campo anterior para TxtSoma + numero dinamico com base na quantidade de Campso gerados
+					    document.getElementById("IdNull").id="TxtSoma" + i;
+					    
+				};
+
+			//Gerar o Botão "Soma" após os campos:
+			document.write('</br><div class="Center"><a href="#" onclick="Somar()" id=BtnGerarCampos class="myButton Center">Somar</a><link href="css/style.css" rel="stylesheet">');
 				
-				//Gerar novo campo com id padrão "IdNull"	
-			    document.write('<div><input id="IdNull" type="text"> <br></div>');
-			    
-			    //Redefinir o id do campo anterior para TxtSoma + numero dinamico com base na quantidade de Campso gerados
-			    document.getElementById("IdNull").id="TxtSoma" + i;
-			    
-			    //alert("Rodou " + i + "° vez" );
-		};
+		}
 
+		else{
 
+				window.alert("Gentileza informar um valor numérico.");
 
-		//Gerar o Botão "Soma" após os campos:
-		document.write('</br><div><input type=button value="Somar" onclick=Somar()></div>');
+		}
+
  }
 
 function Somar(){
 
- 		//valor total da soma 
+ 		//variavel que recebe o valor total da soma.
   		SomaTotal = 0;
 
- 		//Alert de confirmação que a função foi executada
- 		//window.alert("Função soma executada");
-   		
-   		//Loop de repetição para coletar os valore dos campos e somar à variavel SomaTotal
-   		for (var x = 1; x <= camp; x++) {
+ 		//Loop de repetição para coletar os valores dos campos e somar à variavel SomaTotal.
+   		for (var i = 1; i <= camp; i++) {
     			
     			//if para validar se os dados inseridos nos campos são invalidos
-    			if (isNaN(document.getElementById("TxtSoma"+x).value) || document.getElementById("TxtSoma"+x).value == ""){
-			    			
-		    			//Retornoar mensagem de erro 
-		    			window.alert("Os dados informados no " + x +"° campo são inválidos ou estão em branco. Gentileza itulizar apenas numeros para viabilizar a operação")
+    			if (isNaN(document.getElementById("TxtSoma"+i).value) || document.getElementById("TxtSoma"+i).value == ""){
+			    		
+			    		//caso dados sejam inálidos.	
 		    			SomaTotal = NaN;
 		    			break;
 			    }
 			    
 			    //else (caso os dados sejam válidos)
 			    else{
-			    	 		//somar o valor do campo tratado ao valor da variavel SomaTotal
-			    			SomaTotal = SomaTotal + parseInt(document.getElementById("TxtSoma"+ x).value);    			    						    			
+
+		    	 		//somar o valor do campo tratado ao valor da variavel SomaTotal
+		    			SomaTotal = SomaTotal + parseInt(document.getElementById("TxtSoma"+ i).value);    			    						    			
+
 			    }
 
    		}
 
    		if (!isNaN(SomaTotal)) {
-   		 window.alert("O valor total da soma dos " + camp + " campos é: " + SomaTotal);
-   		 SomaTotal = 0;
+				
+				window.alert("A soma dos " + camp + " campos é: " + SomaTotal + ".");
+				SomaTotal = 0;
+				
    		 } 
 
    		 else{
-   		 	window.alert("Não foi possivel realizar a operação, reveja os dados informados no " + x + "° campo");
-   		 	SomaTotal = 0;
+	   		 	window.alert("Não foi possivel realizar a operação, \nDados inválidos (não numérico ou vazio) no  " + i + "° campo.");
+	   		 	SomaTotal = 0;
    		 }
 
  }
